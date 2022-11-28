440673418e9330873b0aa096e18b5bbfe7765026
irfan <engr.irfandayan@gmail.com>
2022-11-28 16:01:52 +0500

    Add formik form with yup validation

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import Dashboard from './scenes/dashboard';
 import Team from './scenes/team';
 import Contacts from './scenes/contacts';
 import Invoices from './scenes/invoices';
+import Form from './scenes/form';
 
 // import Bar from './scenes/bar';
-// import Form from './scenes/form';
 // import Line from './scenes/line';
 // import Pie from './scenes/Pie';
 // import FAQ from './scenes/faq';
@@ -34,8 +34,8 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
-              {/* <Route path="/form" element={<Form />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/faq" element={<FAQ />} /> */}
